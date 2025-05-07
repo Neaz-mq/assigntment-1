cf93231d88779b4676815c0c2e57250ba9a1e887
Neaz-mq <neazmorshed666@gmail.com>
2025-05-07 22:47:25 +0600

    first commit

---
+++ assignment.ts
@@ -0,0 +1,85 @@
+{
+    
+    function formatString(input: string, toUpper?: boolean): string {
+        if (toUpper === undefined || toUpper === true) {
+          return input.toUpperCase();
+        } else {
+          return input.toLowerCase();
+        }
+      }
+
+
+      function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+        return items.filter(item => item.rating >= 4);
+      }
+
+
+      function concatenateArrays<T>(...arrays: T[][]): T[] {
+        return ([] as T[]).concat(...arrays);
+      }
+
+
+      class Vehicle {
+        private make: string;
+        private year: number;
+      
+        constructor(make: string, year: number) {
+          this.make = make;
+          this.year = year;
+        }
+      
+        getInfo(): string {
+          return `Make: ${this.make}, Year: ${this.year}`;
+        }
+      }
+      
+      class Car extends Vehicle {
+        private model: string;
+      
+        constructor(make: string, year: number, model: string) {
+          super(make, year);
+          this.model = model;
+        }
+      
+        getModel(): string {
+          return `Model: ${this.model}`;
+        }
+      }
+
+
+
+      function processValue(value: string | number): number {
+        if (typeof value === "string") {
+          return value.length;
+        } else {
+          return value * 2;
+        }
+      }
+
+
+      
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
